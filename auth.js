b67f6674933e61def2a949ee51b6c3e9868e23bf
Iss1937 <islam-1937@mail.ru>
2023-06-25 16:02:18 +0300

    проверка полей ввода авторизации

--- auth.js
+++ auth.js
@@ -1,3 +1,28 @@
 const addForm = document.querySelector('.add-form');
-//Скрыть форму
+
+const loginName = document.querySelector('.login-form-name');
+const loginPassword = document.querySelector('.login-form-pass');
+const loginButton = document.querySelector('.login-form-button');
+
+export { addForm, loginName, loginPassword, loginButton };
+
+//Скрыть форму добавления комментария
 addForm.classList.add('hidden');
+
+//Функция проверяет заполнены поля или нет
+loginButton.addEventListener('click', () => {
+  checkInputs();
+});
+
+function checkInputs() {
+  const loginNameValue = loginName.value.trim();
+  const loginPasswordValue = loginPassword.value.trim();
+  if (loginNameValue === '') {
+    loginName.placeholder = 'Напишите логин';
+    loginName.classList.add('error');
+  }
+  if (loginPasswordValue === '') {
+    loginPassword.placeholder = 'Напишите пароль';
+    loginPassword.classList.add('error');
+  }
+};
